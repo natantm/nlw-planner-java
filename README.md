6491a8e8f572d82c744bb89100a95280c4527aaf
Natan <natant.manique03@outlook.com>
2024-07-14 12:27:27 -0300

    Tratamento de erros, adição do swagger-ui e revisão do README.md

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-## Features extras
-- Exceptions
-  - Com tratativas de erro da aplicação
-- Teste unitarios
+# Planner
+
+## Descrição
+Planner é uma API para gerenciamento de viagens desenvolvida durante a NLW da Rocketseat. A API permite criar viagens, convidar participantes, registrar atividades e criar links para cada viagem. O projeto utiliza Java com Spring Boot e um banco de dados em memória H2Database.
+
+## Funcionalidades
+- Criar uma nova viagem
+- Convidar participantes para uma viagem
+- Registrar atividades para uma viagem
+- Criar links para uma viagem
+
+## Modificações
+- Uso de annotations para validações de data e email
+- Tratamento de exceções
+- Uso do OpenAI para facilitar documentação
+   - Quando estiver rodando a API, você pode acessar o link: [Swagger UI](http://localhost:5001/swagger-ui/index.html) para ver os endpoints e entradas de dados
---
+++ src/main/java/com/rocketseat/planner/exceptions/Exceptions.java
@@ -0,0 +1,41 @@
+package com.rocketseat.planner.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class Exceptions {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity ErroInterno(ConstraintViolationException ex){
+        var msg = ex.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(msg);
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
